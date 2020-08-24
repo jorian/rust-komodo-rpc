945951ff674e7ef9dadc1ab8d1cb8b7523bfb1d6
jorian <jorian@outlook.com>
2020-08-24 07:31:48 +0200

    add README, little prototyping

---
+++ README.md
@@ -0,0 +1,17 @@
+# rust-komodo-rpc
+
+A continuation of komodorpc-rust-client, heavily inspired by 
+
+### Goals
+- Learn Rust
+  - workspaces
+- Simplify codebase
+- Do more than just be a RPC lib
+
+### Questions
+- Why is there a Queryable trait?
+- `#[serde(with = "bitcoin::util::amount::serde::as_btc")]` ?
+- `pub mod serde_hex` returns a `FromHex`, which is a trait. But the trait
+doesn't specifically say it's a Vec<u8>. How does this work? 
+Is it because `FromHex` is `Sized` it could be anything, 
+as long as it is `Sized`? 
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,67 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+mod client {
+    // extern crate jsonrpc;
+
+    use jsonrpc;
+    use std::{io, result};
+    use crate::error::Error;
+
+    pub type Result<T> = result::Result<T, Error>;
+
+    #[derive(Clone, Debug)]
+    pub enum Auth {
+        UserPass(String, String),
+        ConfigFile(path),
+    }
+
+    impl Auth {
+        fn get(self) -> Result<(String, String)> {
+            match self {
+                Auth::ConfigFile(path) => {
+                    // read config file based on system OS.
+
+                    Ok(("a".to_string(), "b".to_string()))
+                },
+                Auth::UserPass(rpcuser, rpcpass) => Ok((rpcuser, rpcpass))
+            }
+        }
+    }
+
+    pub struct Client {
+        client: jsonrpc::client::Client,
+    }
+
+    impl Client {
+        pub fn new(url: String, auth: Auth) -> Result<Self> {
+            let (rpcuser, rpcpass) = auth.get()?;
+            Ok(Client {
+                client: jsonrpc::client::Client::new(url, user, pass)
+            })
+        }
+    }
+}
+
+mod error {
+    use std::io;
+
+    #[derive(debug)]
+    pub enum Error {
+        JsonRPC(jsonrpc::Error),
+        IOError(io::Error)
+    }
+
+    impl From<jsonrpc::Error> for Error {
+        fn from(e: jsonrpc::Error) -> Error {
+            Error::JsonRPC(e)
+        }
+    }
+
+    impl From<io::Error> for Error {
+        fn from(e: io::Error) -> Error {
+            Error::Io(e)
+        }
+    }
+}
